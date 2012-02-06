1b186fa4bd3066fe6512e3e31bc14f2dd47dfc8c
xdite <xuite.joke@gmail.com>
2012-02-06 19:45:42 +0800

    change notice_message API

--- lib/bootstrap_helper/helper.rb
+++ lib/bootstrap_helper/helper.rb
@@ -26,15 +26,14 @@ module BootstrapHelper
 
     end
     
+    
     def notice_message
       flash_messages = []
-
       flash.each do |type, message|
         type = :success if type == :notice
-        text = content_tag(:div, link_to("x", "#", :class => "close") + content_tag(:p,message), :class => "alert-message #{type}", "data-alert" => "alert")
+        text = content_tag(:div, link_to("x", "#", :class => "close") + message, :class => "alert-message #{type}", "data-alert" => "alert")
         flash_messages << text if message
       end
-
       flash_messages.join("\n").html_safe
     end
 
