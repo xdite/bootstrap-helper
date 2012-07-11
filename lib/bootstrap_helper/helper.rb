de4beec0ab648204b4b8ec6974eb7a6525d3962e
juan david pastas <juandavid1707@gmail.com>
2012-07-11 11:22:38 -0500

    Added buttons helpers, usage: btn 'A text', a_path_helper, class: 'my-class', ... btn_primary 'A text', a_path_helper, class: 'my-class', ... btn_danger 'A text', a_path_helper, class: 'my-class', ... ... is intended for additional options

--- lib/bootstrap_helper/helper.rb
+++ lib/bootstrap_helper/helper.rb
@@ -145,6 +145,17 @@ module BootstrapHelper
 
       return ul.to_s
     end
+    
+    def btn(text, path, type = '', options={})
+      css = options.delete(:class) || options.delete('class')
+      link_to text, path, {class: 'btn ' + type + ' ' + css}.merge(options)
+    end
+
+    %w(primary info success warning danger inverse).each do |type|
+      define_method 'btn_' + type do |text, path, options = {}|
+        btn text, path, 'btn-' + type, options
+      end
+    end
 
     # Composite pattern
     class TagNode
