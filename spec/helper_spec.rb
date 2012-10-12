46debbc805255875a73cd2f67f3555ed6055cc35
xdite <xdite@rocodev.com>
2012-10-12 08:48:53 +0800

    add some pending test

--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -1,4 +1,30 @@
 require 'spec_helper'
+
 describe BootstrapHelper::Helper do
-  pending "write it"
+  describe "yield_or_default" do
+    it "should return yield_name" do
+      yield_or_default(:xxx).should == :xxx
+    end
+
+    it "should return default message" do
+      yield_or_default(nil).should == ""
+    end
+
+  end
+
+  describe "render_page_title" do
+    pending
+  end
+
+  describe "render_body_tag" do
+    pending
+  end
+
+  describe "notice_message" do
+    pending
+  end
+
+  describe "render_list" do
+    pending
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,7 @@ require 'bundler/setup'
 
 require 'bootstrap_helper'
 
+include BootstrapHelper::Helper
+
 RSpec.configure do |config|
 end
