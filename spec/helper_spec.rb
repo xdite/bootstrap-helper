c4936b795123cbfd3c34941a4df67718646867dc
xdite <xdite@rocodev.com>
2012-10-12 08:35:39 +0800

    init rspec

---
+++ spec/helper_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+describe BootstrapHelper::Helper do
+  pending "write it"
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'bootstrap_helper'
+
+RSpec.configure do |config|
+end
