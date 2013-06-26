ca9bf7e100081408d5017e446073a6a61cda6eb4
xdite <xuite.joke@gmail.com>
2013-06-26 22:53:08 +0800

    Rails 4.0.0 official

--- Gemfile
+++ Gemfile
@@ -5,4 +5,4 @@ gemspec
 
 
 
-gem "simple_form", "3.0.0.beta1"
+gem "simple_form", "3.0.0.rc"
--- lib/bootstrap_helper/version.rb
+++ lib/bootstrap_helper/version.rb
@@ -1,5 +1,5 @@
 module BootstrapHelper
   module Rails
-    VERSION = "4.2.2.2"
+    VERSION = "4.2.2.3"
   end
 end
